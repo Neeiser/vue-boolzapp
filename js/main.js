fbe7578e840181f88b5b147ebe42fe8e6baa63fb
Neeiser <stortini.study@gmail.com>
2022-03-16 21:53:44 +0100

    FIXED FINALLY! It was @keydown.enter not @keydown-enter... 4 hours for this stupid mistake...

--- js/main.js
+++ js/main.js
@@ -244,11 +244,11 @@ const app = new Vue({
     methods:{
         sendMessage: function(){
             if(this.newMessage !== ''){
-                this.contacts[elementActive].msg.push({
+                this.contacts[this.elementActive].msg.push({
                     textMsg: this.newMessage, timeMsg: '00:00', status:true
-                })
+                });
                 this.newMessage=''
             }
-        }
+        },
     }
 })
