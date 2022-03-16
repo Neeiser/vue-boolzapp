0ab88c313ee5826d73fdef7b83714dd6506d52d1
Neeiser <stortini.study@gmail.com>
2022-03-16 15:18:11 +0100

    stucked

--- js/main.js
+++ js/main.js
@@ -19,54 +19,80 @@ Pensate bene a come strutturare i dati prima di implementare il codice.
 const app = new Vue({
     el:'.app',
     data:{
+        element: 0,
         contacts:[
             {
                 name: 'Michele',
                 lastMsg: 'Ultimo messaggio inviato',
                 lastTime: '12:00',
                 img: 'img/avatar_1.jpg',
+                visible: true,
+                msg: [
+                    {
+                        textMsg: 'Hai portato a spasso il drago di Komodo?',
+                        timeMsg: '15:30',
+                        status: 'sent',
+                    },
+                    {
+                        textMsg: 'Quando esci ricordati di prendere il cianuro per la torta di domani... e non dimenticarti le banane di Bronte.',
+                        timeMsg: '15:35',
+                        status: 'sent',
+                    },
+                    {
+                        textMsg: 'Signora ha sbagliato numero.',
+                        timeMsg: '16:00',
+                        status: 'recived',
+                    }
+                ]
             },
             {
                 name: 'Fabio',
                 lastMsg: 'Ultimo messaggio inviato',
                 lastTime: '12:00',
                 img: 'img/avatar_2.jpg',
+                visible: false,
             },
             {
                 name: 'Samuele',
                 lastMsg: 'Ultimo messaggio inviato',
                 lastTime: '12:00',
                 img: 'img/avatar_3.jpg',
+                visible: false,
             },
             {
                 name: 'Alessandro B.',
                 lastMsg: 'Ultimo messaggio inviato',
                 lastTime: '12:00',
                 img: 'img/avatar_4.jpg',
+                visible: false,
             },
             {
                 name: 'Alessandro L.',
                 lastMsg: 'Ultimo messaggio inviato',
                 lastTime: '12:00',
                 img: 'img/avatar_5.jpg',
+                visible: false,
             },
             {
                 name: 'Claudia',
                 lastMsg: 'Ultimo messaggio inviato',
                 lastTime: '12:00',
                 img: 'img/avatar_6.jpg',
+                visible: false,
             },
             {
                 name: 'Federico',
                 lastMsg: 'Ultimo messaggio inviato',
                 lastTime: '12:00',
                 img: 'img/avatar_7.jpg',
+                visible: false,
             },
             {
                 name: 'Davide',
                 lastMsg: 'Ultimo messaggio inviato',
                 lastTime: '12:00',
                 img: 'img/avatar_8.jpg',
+                visible: false,
             },
         ]
     }
