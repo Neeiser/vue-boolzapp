24b98e4c956da8f221b85fbe96affc2b525ad6a2
Neeiser <stortini.study@gmail.com>
2022-03-16 10:55:54 +0100

    Task comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+/* 
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, 
+visualizzare tutti i messaggi relativi al contatto attivo all’interno del 
+pannello della conversazione.
+Click sul contatto mostra la conversazione del contatto cliccato.
+
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e 
+digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde.
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, 
+l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Consiglio
+Pensate bene a come strutturare i dati prima di implementare il codice.
+*/
+
+
 const app = new Vue({
     el:'.app',
     data:{
