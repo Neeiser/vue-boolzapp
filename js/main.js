5d5974fa94169fb540796d90ef2468bd1c562f2b
Neeiser <stortini.study@gmail.com>
2022-03-17 15:28:05 +0100

    data showDelete

--- js/main.js
+++ js/main.js
@@ -28,16 +28,19 @@ const app = new Vue({
                         textMsg: 'Hai portato a spasso il drago di Komodo?',
                         timeMsg: '15:30',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Quando esci ricordati di prendere il cianuro per la torta di domani... e non dimenticarti le banane di Bronte.',
                         timeMsg: '15:35',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Signora ha sbagliato numero.',
                         timeMsg: '16:00',
                         status: false,
+                        showDelete: false,
                     }
                 ]
             },
@@ -52,16 +55,19 @@ const app = new Vue({
                         textMsg: 'Procurami il sale',
                         timeMsg: '15:30',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Quanto?.',
                         timeMsg: '15:35',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Quanto basta.',
                         timeMsg: '16:00',
                         status: true,
+                        showDelete: false,
                     }
                 ]
             },
@@ -76,21 +82,25 @@ const app = new Vue({
                         textMsg: 'Ciao!',
                         timeMsg: '15:30',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Guarda un po\' chi si rifà vivo...',
                         timeMsg: '15:35',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'In realtà sono morto',
                         timeMsg: '16:00',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Ah! Chiedo scudo...',
                         timeMsg: '15:35',
                         status: false,
+                        showDelete: false,
                     },
                 ]
             },
@@ -105,26 +115,31 @@ const app = new Vue({
                         textMsg: 'Oddio non crederai mai a ciò che è successo!',
                         timeMsg: '15:30',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Racconta, racconta!',
                         timeMsg: '15:32',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Praticamente stavo andando a prendere il latte, quando ad un tratto ho notato che mi sono dimenticato il cellulare a casa.',
                         timeMsg: '16:00',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'e...',
                         timeMsg: '16:35',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'e nulla, sono tornato a casa, l\'ho preso ed ora vado a prendere il latte',
                         timeMsg: '16:40',
                         status: false,
+                        showDelete: false,
                     },
                 ]
             },
@@ -139,11 +154,13 @@ const app = new Vue({
                         textMsg: 'Qual\'è il colmo per un narvalo ?',
                         timeMsg: '15:30',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Tirare sassate agli infedeli.',
                         timeMsg: '15:35',
                         status: false,
+                        showDelete: false,
                     },
                 ]
             },
@@ -158,46 +175,55 @@ const app = new Vue({
                         textMsg: 'L\'Inquisizione era l\'istituzione ecclesiastica fondata dalla Chiesa cattolica per indagare, mediante un apposito tribunale, i sostenitori di teorie considerate contrarie all\'ortodossia cattolica (le cosiddette eresie).',
                         timeMsg: '15:30',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Ok e con questo? Perchè mi hai scritto ciò!?',
                         timeMsg: '15:35',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Perchè sono diventato un templare',
                         timeMsg: '16:00',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Il mio cuore e la mia spada ora appartengono alla mia causa. Possa l\'eterna fede vegliare sulla mia corciata.',
                         timeMsg: '16:00',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'A me puzza di ipocrisia; fino a ieri stavi bestemmiando...',
                         timeMsg: '15:35',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'La misericordia del sommo non conosce limiti.',
                         timeMsg: '16:00',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Addio Gerardo.',
                         timeMsg: '16:00',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'In realtà mi chiamerei Claudia...',
                         timeMsg: '16:00',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'La fede non conosce nomi.',
                         timeMsg: '16:00',
                         status: true,
+                        showDelete: false,
                     },
                 ]
             },
@@ -212,6 +238,7 @@ const app = new Vue({
                         textMsg: 'Condividi anche te questo messaggio e avrai fortuna per 50 anni: "L\'impero ottomano aveva ragione, le bruschette è meglio tostarle al forno e non in padella."',
                         timeMsg: '15:30',
                         status: true,
+                        showDelete: false,
                     },
                 ]
             },
@@ -226,26 +253,31 @@ const app = new Vue({
                         textMsg: 'Davide, ti facevo più alto!',
                         timeMsg: '15:30',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Ma se non ci siamo mai visti!',
                         timeMsg: '15:35',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Tu pensi? Secondo te chi è quello che ti guarda dalla finestra mentre dormi?',
                         timeMsg: '16:00',
                         status: true,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Ma io non ho finestre a casa...',
                         timeMsg: '16:00',
                         status: false,
+                        showDelete: false,
                     },
                     {
                         textMsg: 'Perfetto, significa che ho guardato per tutto questo tempo la finestra sbagliata.',
                         timeMsg: '15:30',
                         status: true,
+                        showDelete: false,
                     },
                 ]
             },
